3c9cd7c7e29829552d99ba69998f6965fae84fce
Yann <mail@it.yann.red>
2020-09-21 18:57:27 +0200

    Modifcation de l'enregistrement de produit dans l interface admin suite a l'utilisation de FormData coté front

--- src/Controller/EnregistrerProduitController.php
+++ src/Controller/EnregistrerProduitController.php
@@ -9,11 +9,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Dotenv\Dotenv;
 
 class EnregistrerProduitController extends AbstractController
 {
     const ACCES_ERROR_CODE = 403;
-    const ERROR_CODE=400;
+    const ERROR_CODE = 400;
 
     /**
      * @Route("/admin/enregistrerProduit", name="enregistrer_produit", methods={"POST"})
@@ -27,43 +28,35 @@ class EnregistrerProduitController extends AbstractController
         for ($i = 0; $i < count($roles); $i++) {
             if ($roles[$i] == "ROLE_ADMIN") {
 
-                if ($content = $request->getContent()) {
-                    $json = json_decode($content, true);
-                    if (isset($json['nom']) &&
-                        isset($json['description']) &&
-                        isset($json['prix']) &&
-                        isset($json['promo']) &&
-                        isset($json['categorie'])
-                    ) {
-                        $repository = $this->getDoctrine()->getRepository(CategorieProduit::class);
-                        $categorie = $repository->findBy(['nom' => $json['categorie']]);
-                        $produit = new Produit();
-                        $produit->setCategorieProduit($categorie[0]);
-                        $produit->setNom($json['nom']);
-                        $produit->setDescription($json['description']);
-                        $produit->setPrix($json['prix']);
-                        $produit->setPromo($json['promo'] == "true");
-                        if(isset($_FILES['photo'])){
-                            $dossier = '';
-                            $fichier=time();
-                            $extension = strrchr($_FILES['photo']['name'], '.');
-                            $extensionsAcceptees = array('.png', '.gif', '.jpg', '.jpeg');
-                            if(!in_array($extension, $extensionsAcceptees)){
-                                return new JsonResponse(['Extension non acceptee'], self::ERROR_CODE);
-                            }
-                            if(!move_uploaded_file($_FILES['photo']['tmp_name'], $dossier . $fichier . $extension)){
-                                return new JsonResponse(['Erreur dans la requete'], self::ERROR_CODE);
-                            }
-                            $produit->setPhoto($fichier . $extension);
-                        } else {
-                            $produit->setPhoto("/Produits/0000.png");
-                        }
-                        $entityManager->persist($produit);
-                        $entityManager->flush();
+                $parametresRequest = [];
+                if ($request->request->get('nom') &&
+                    $request->request->get('categorie') &&
+                    $request->request->get('description') &&
+                    $request->request->get('prix') &&
+                    $request->request->get('promo'))
+                {
+                    $parametresRequest['nom'] = (string)$request->request->get('nom');
+                    $parametresRequest['categorie'] = (string)$request->request->get('categorie');
+                    $parametresRequest['description'] = (string)$request->request->get('description');
+                    $parametresRequest['prix'] = (float)$request->request->get('prix');
+                    $parametresRequest['promo'] = (string)$request->request->get('promo');
+                    $repository = $this->getDoctrine()->getRepository(CategorieProduit::class);
+                    $categorie = $repository->findBy(['nom' => $parametresRequest['categorie']]);
+                    $produit = new Produit();
+                    $produit->setCategorieProduit($categorie[0]);
+                    $produit->setNom($parametresRequest['nom']);
+                    $produit->setDescription($parametresRequest['description']);
+                    $produit->setPrix($parametresRequest['prix']);
+                    $produit->setPromo($parametresRequest['promo'] == 'true');
+
+                        $produit->setPhoto("/Produits/0000.png");
+
+                    $entityManager->persist($produit);
+                    $entityManager->flush();
 
 
-                        return new JsonResponse(['reponse' => 'Produit enregistré']);
-                    }
+                    return new JsonResponse(['reponse' => 'Produit enregistré']);
+
                 }
             }
         }
